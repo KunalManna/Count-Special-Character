d2d237fc78c268a205c3d0111e97c29fa724f943
KunalManna <117971514+KunalManna@users.noreply.github.com>
2022-11-15 11:35:34 +0530

    Update Special_Char_Count.py

--- Special_Char_Count.py
+++ Special_Char_Count.py
@@ -1,5 +1,5 @@
 #Count the Special charcter in the given string except space 
-s=input("Enter the string:\n")
+s=input("Enter the string:\n")        #aa a234Bc@@# Sad$%%  hsgv*
 count=0
 for i in s:
     if not ((i>='0' and i<='9') or (i>='A' and i<='Z') or (i>='a' and i<='z') or (i==' ')):
